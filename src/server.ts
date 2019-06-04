3cb5461afda9efd83a4b0b7e2ca3790771c879c4
이진석 <sonaky47@gmail.com>
2019-06-04 11:58:31 +0900

    이것저것

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 
 import express from 'express';
 import db from './database/db';
+
 import productController from './controllers/product';
 
 class Server {
@@ -22,8 +23,37 @@ class Server {
     );
   }
 
+  ensureDb() {
+    return new Promise((resolve, reject) => {
+      let counter = 0;
+      const tryConnect = async () => {
+        try {
+          await db.authenticate();
+          resolve();
+        } catch (e) {
+          counter++;
+          console.log(`db connection failed ${counter}`);
+          if (counter > 5) {
+            reject(new Error('Failed after 5 retries'));
+            return;
+          }
+          setTimeout(tryConnect, 10);
+        }
+      };
+      tryConnect();
+    });
+  }
+
   middleware(): void {
     const { app } = this;
+    app.use(async (req, res, next) => {
+      try {
+        await this.ensureDb();
+        next();
+      } catch (e) {
+        next(e);
+      }
+    });
     app.get('/', (req: express.Request, res: express.Response) => {
       res.send('hello world');
     });
